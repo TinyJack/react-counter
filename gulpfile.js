5a65b01ffd5fc9c632d88d1b4e033c322124bd01
xt <khlopenuk.ta@gamil.com>
2017-03-12 18:40:28 +0500

    gulp conf

--- gulpfile.js
+++ gulpfile.js
@@ -3,19 +3,39 @@ const webpack = require('webpack');
 const uglify = require('gulp-uglify');
 const stylus = require('gulp-stylus');
 const plumber = require('gulp-plumber');
+const named = require('vinyl-named');
 const webpackStream = require('webpack-stream');
 const webserver = require('gulp-webserver');
 
+const debug = process.env.NODE_ENV !== 'production';
+
 gulp.task('js', done => {
-    return gulp.src('src/*.js')
+    return gulp.src('src/js/**/*.js')
         .pipe(plumber())
-        .pipe(webpackStream())
+        .pipe(webpackStream({
+            watch: debug,
+            output: {
+                filename: 'index.js'
+            },
+            module: {
+                 loaders: [ { test: /\.js?$/, loader: 'babel' } ],
+                 query: {
+                        presets: [
+                            require.resolve('babel-preset-es2015'),
+                            require.resolve('babel-preset-react'),
+                        ]
+                    },
+               
+            }
+             
+        }))
         .pipe(gulp.dest('dist/js/'));
 });
 
 
 gulp.task('webserver', function() {
   gulp.src('./dist')
+    .pipe(plumber())
     .pipe(webserver({
       livereload: true,
     }));
---
+++ src/js/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+/** Main Application node */
+// const node = 
+//     <div>
+//     	Hello, world!
+//     </div>;
+
+/** Render Application node */
+// ReactDOM.render(node, document.getElementById('app'));
